6007193b4698cea9c71f22998b47c1caf5868ce2
hugoisrr <84rami@gmail.com>
2019-06-04 14:26:02 +0200

    clean client

--- client/src/actions/types.js
+++
@@ -1,4 +0,0 @@
-export const SET_ALERT = "SET_ALERT";
-export const REMOVE_ALERT = "REMOVE_ALERT";
-export const GET_PROJECT = "GET_PROJECT";
-export const PROJECT_ERROR = "PROJECT_ERROR";
--- client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import alert from "./alert";
-import project from "./project";
-
-export default combineReducers({
-  alert,
-  project
-});
