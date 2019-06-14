a4ae7f157c6181141253e4dfef6fa2471f875125
hugoisrr <84rami@gmail.com>
2019-06-14 13:48:32 +0200

    unipi setup

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 const connectUDP = require("./config/udpServer");
 const socket = require("./config/socket");
+const evok = require("unipi-evok");
 
 const app = express();
 
@@ -32,5 +33,43 @@ const server = app.listen(port, () =>
 // Setup Socket io connection
 const io = socket.init(server);
 io.on("connection", socket => {
-  console.log("Client connected", socket.id);
+  console.log("Client connected to Server", socket.id);
 });
+
+// Neuron connection
+// const neuron = new net.Socket();
+// neuron.connect(8080, "192.168.1.234", () => console.log("Neuron connected"));
+// const ioNeuron = socket.init(neuron);
+// io.on("connection", socket => {
+//   console.log("Client connected to Neuron", socket.id);
+// });
+const unipi = new evok({
+  host: "192.168.1.234",
+  restPort: 80,
+  wsPort: 8080
+});
+
+unipi
+  .on("connected", () => {
+    console.log("Unipi connected");
+  })
+  .on("message", function(chunk) {
+    const circuit = chunk[0].circuit;
+    const value = chunk[0].value;
+    console.log(`circuit: ${circuit} - value: ${value}`);
+  })
+  .on("open", () => {
+    send({
+      cmd: "set",
+      dev: "relay",
+      circuit: "2_01",
+      value: "1"
+    });
+  })
+  .connect();
+
+console.log(unipi.wsUrl()); // ws://192.168.1.234:8080/ws
+
+// unipi.get("/rest/all").then(devices => {
+//   console.log("devices:", devices);
+// });
